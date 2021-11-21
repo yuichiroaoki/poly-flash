529a5314d6cd6315ffe34c8a64ddcfcc4a2c6ab5
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-11-21 10:52:03 +0900

    rename contract

--- test/flashloan.test.ts
+++ test/flashloan.test.ts
@@ -1,13 +1,13 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { DAI_WHALE, dodoV2Pool, erc20Address, quickRouter } from "../constrants/addresses";
-import { ERC20Mock, ProdV2, ProdV2__factory } from "../typechain";
+import { DAI_WHALE, dodoV2Pool, erc20Address, uniswapRouter } from "../constrants/addresses";
+import { ERC20Mock, Flashloan, Flashloan__factory } from "../typechain";
 import { deployContractFromName, getBigNumber, getERC20ContractFromAddress } from "../utils";
 import { impersonateFundErc20 } from "../utils/token";
 
-describe("ProdV2", () => {
-	let Production: ProdV2;
+describe("Flashloan", () => {
+	let Flashloan: Flashloan;
 	let owner: SignerWithAddress;
 	let addr1: SignerWithAddress;
 	let addr2: SignerWithAddress;
@@ -19,11 +19,11 @@ describe("ProdV2", () => {
 	beforeEach(async () => {
 		[owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-		Production = await deployContractFromName(
-			"ProdV2",
-			ProdV2__factory
+		Flashloan = await deployContractFromName(
+			"Flashloan",
+			Flashloan__factory
 		);
-		await Production.deployed();
+		await Flashloan.deployed();
 
 		USDC = await getERC20ContractFromAddress(erc20Address.USDC)
 		DAI = await getERC20ContractFromAddress(erc20Address.DAI)
@@ -33,77 +33,77 @@ describe("ProdV2", () => {
 	describe("flashloan", () => {
 		it("should revert flashloan when there's no arbitrage opportunity.", async () => {
 			await expect(
-				Production.dodoFlashLoan({
+				Flashloan.dodoFlashLoan({
 					flashLoanPool: dodoV2Pool.WETH_USDC,
 					loanAmount: getBigNumber(1, 6),
 					firstRoutes: [{
 						path: [erc20Address.USDC, erc20Address.DAI],
-						router: quickRouter,
+						router: uniswapRouter.quickRouter,
 					}],
 					secondRoutes: [{
 						path: [erc20Address.DAI, erc20Address.USDC],
-						router: quickRouter,
-					}]
+						router: uniswapRouter.quickRouter
+					}],
 				})
 			).to.be.revertedWith("Not enough amount to return loan");
 		});
 
 		it("should execute flashloan.", async () => {
-			await impersonateFundErc20(USDC, DAI_WHALE, Production.address, "100.0", 6);
+			await impersonateFundErc20(USDC, DAI_WHALE, Flashloan.address, "100.0", 6);
 			await expect(
-				Production.dodoFlashLoan({
+				Flashloan.dodoFlashLoan({
 					flashLoanPool: dodoV2Pool.WETH_USDC,
 					loanAmount: getBigNumber(1, 6),
 					firstRoutes: [{
 						path: [erc20Address.USDC, erc20Address.DAI],
-						router: quickRouter,
+						router: uniswapRouter.quickRouter
 					}],
 					secondRoutes: [{
 						path: [erc20Address.DAI, erc20Address.USDC],
-						router: quickRouter,
+						router: uniswapRouter.quickRouter
 					}]
 				})
 			)
-			.emit(Production, "SwapFinished")
-			.emit(Production, "SentProfit");
+				.emit(Flashloan, "SwapFinished")
+				.emit(Flashloan, "SentProfit");
 			const balance = await USDC.balanceOf(owner.address);
 			expect(balance.gt(getBigNumber(80, 6))).to.be.true;
 		});
 
-		it("should execute flashloan like production with gas limit", async () => {
+		it("should execute flashloan like Flashloan with gas limit", async () => {
 			await expect(
-				Production.dodoFlashLoan({
+				Flashloan.dodoFlashLoan({
 					flashLoanPool: dodoV2Pool.WETH_USDC,
 					loanAmount: getBigNumber(1, 6),
 					firstRoutes: [{
 						path: [erc20Address.USDC, erc20Address.DAI],
-						router: quickRouter,
+						router: uniswapRouter.quickRouter
 					}],
 					secondRoutes: [{
 						path: [erc20Address.DAI, erc20Address.USDC],
-						router: quickRouter,
+						router: uniswapRouter.quickRouter
 					}]
 				}, { gasLimit: 1000000 })
 			).to.be.revertedWith("Not enough amount to return loan");
 		});
 
 		it("should execute flashloan with initial token amount", async () => {
-			await impersonateFundErc20(USDC, DAI_WHALE, Production.address, "100.0", 6);
+			await impersonateFundErc20(USDC, DAI_WHALE, Flashloan.address, "100.0", 6);
 			await expect(
-				Production.dodoFlashLoan({
+				Flashloan.dodoFlashLoan({
 					flashLoanPool: dodoV2Pool.WETH_USDC,
 					loanAmount: getBigNumber(1, 6),
 					firstRoutes: [{
 						path: [erc20Address.USDC, erc20Address.DAI],
-						router: quickRouter,
+						router: uniswapRouter.quickRouter,
 					}],
 					secondRoutes: [{
 						path: [erc20Address.DAI, erc20Address.USDC],
-						router: quickRouter,
+						router: uniswapRouter.quickRouter,
 					}]
 				}, { gasLimit: 1000000 })
 			)
-			.emit(Production, "SentProfit");
+				.emit(Flashloan, "SentProfit");
 			const balance = await USDC.balanceOf(owner.address);
 			expect(balance.gt(getBigNumber(80, 6))).to.be.true;
 		});
