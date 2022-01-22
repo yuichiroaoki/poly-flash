37a21daeadceac3292f9b68a340cdd87acd5da6c
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-01-22 14:29:36 +0900

    update a deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,39 @@
-import { ethers } from "hardhat";
-import { Flashloan, Flashloan__factory } from "../typechain";
+import { ethers, upgrades } from "hardhat";
+import { uniswapRouter, UniswapV3poolFee } from "../constants/addresses";
+import {
+  Router,
+  Flashloan,
+  Flashloan__factory,
+  Router__factory,
+} from "../typechain";
+import { deployContractFromName } from "../utils";
 
 async function main() {
-	let Flashloan: Flashloan;
+  const factory = (await ethers.getContractFactory(
+    "Router"
+  )) as Router__factory;
+  const Router = (await upgrades.deployProxy(
+    factory,
+    [Object.values(uniswapRouter), UniswapV3poolFee],
+    {
+      initializer: "initialize",
+    }
+  )) as Router;
+  await Router.deployed();
 
-	const factory = (
-		await ethers.getContractFactory("Flashloan")
-	) as Flashloan__factory;
+  const Flashloan: Flashloan = await deployContractFromName(
+    "Flashloan",
+    Flashloan__factory,
+    [Router.address]
+  );
+  await Flashloan.deployed();
 
-	Flashloan = await factory.deploy();
-	await Flashloan.deployed();
-
-	console.log("contract deployed to:", Flashloan.address);
+  console.log("contract deployed to:", Flashloan.address);
 }
 
 main()
-	.then(() => process.exit(0))
-	.catch((error) => {
-		console.error(error);
-		process.exit(1);
-	});
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
