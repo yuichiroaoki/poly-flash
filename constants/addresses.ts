37b6faab05fd63c5b0d9c57c24fbc9cb54ce4f08
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-04-17 09:50:47 +0900

    remove debug.sol

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -59,12 +59,6 @@ export const UniswapV3poolFee: PoolInfo[] = [
   { base: erc20Address.WETH, quote: erc20Address.WMATIC, fee: 500 },
 ];
 
-/**
- * deployed Router contract address
- * https://polygonscan.com/address/0x112aae1218e91392293cb3e63d4f9e7c9c376d2c
- */
-export const routerAddress = "0x112Aae1218E91392293cb3E63d4f9E7C9c376d2c";
-
 export const DODOApprove = {
   ETH: "0xCB859eA579b28e02B87A1FDE08d087ab9dbE5149",
   BSC: "0xa128Ba44B2738A558A1fdC06d6303d52D3Cef8c1",
